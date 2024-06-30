18654ea8a8ff9f07c7f3882714a64aeb850bc0e5
sharad <sharadit2011@gmail.com>
2024-06-30 21:35:17 +0530

    max-sub- init

---
+++ 05-maximum-subarray/01-bruteforce-solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        
+nums = [1,1,5]
+print(nums)                  
+obj = Solution() 
+output= obj.maxSubArray(nums)
+print(output)           
